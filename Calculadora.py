afc68f50bafa6f87c888d07930f4a51f44b76558
Diegotmi <dihdhstmi@gmail.com>
2019-03-11 21:40:54 -0300

    recebe a entrada de 2 numeros pelo usuario, e garante que serao inteiros

--- Calculadora.py
+++ Calculadora.py
@@ -1,2 +1,4 @@
 primeiro_numero = int(input('Insira seu primeiro número:'))
 segundo_numero = int(input( 'Insira o segundo número:') )
+
+print('Soma dos numeros é:', primeiro_numero + segundo_numero)
