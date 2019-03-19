a19ebb999333e529a39a12dbc3c81e72c0d86854
Diegotmi <dihdhstmi@gmail.com>
2019-03-19 19:40:46 -0300

    Porcentagem premiaçao

---
+++ Premio.py
@@ -0,0 +1,11 @@
+#A importância de R$ 780.000,00 será dividida entre os três primeiros colocados de um concurso,
+# da seguinte forma: 1º lugar receberá 50%, 2º lugar receberá 30% e 3º lugar receberá 20%.
+# Construa um algoritmo que calcule o valor a ser pago para um dos três colocados.
+
+valor_premio = 780000.00
+
+primeiro =(valor_premio *50 )/100
+segundo = (valor_premio *30) /100
+terceiro = (valor_premio *20) /100
+
+print('Valor do primeiro premio:',primeiro,'\n'+'Valor do segundo premio:',segundo,'\n''Valor do terceiro premio:',terceiro)
